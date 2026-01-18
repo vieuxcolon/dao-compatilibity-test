590b763a647e1fb8040a438206ddfac7aa71226e
vieuxcolon <vieuxcolon@hotmail.fr>
2026-01-18 17:12:34 +0100

    Update run-all.sh

--- run-all.sh
+++ run-all.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 set -e
 
-# Build Docker image
 echo "===== Building Docker container ====="
 docker build -t dao-compat-test .
 
-# Run container to test compatibility
-echo "===== Running compatibility test ====="
+echo "===== Running all compatibility tests ====="
 docker run --rm dao-compat-test
- dao-compat-test
