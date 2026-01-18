48c91e00a4b712c604f221629fb0d68f3fbeddb7
vieuxcolon <vieuxcolon@hotmail.fr>
2026-01-19 00:57:17 +0100

    Create WalletConnect.js

---
+++ frontend/src/components/WalletConnect.js
@@ -0,0 +1,11 @@
+export default function WalletConnect({ connectWallet, account }) {
+  return (
+    <div>
+      {account ? (
+        <p>Connected: {account}</p>
+      ) : (
+        <button onClick={connectWallet}>Connect Wallet</button>
+      )}
+    </div>
+  );
+}
