4961311b985786ad5576da5eb758086263f9177e
vieuxcolon <vieuxcolon@hotmail.fr>
2026-01-19 12:40:46 +0100

    Create hardhat.config.mjs

---
+++ hardhat.config.mjs
@@ -0,0 +1,13 @@
+// hardhat.config.mjs
+import "@nomicfoundation/hardhat-toolbox";
+import dotenv from "dotenv";
+dotenv.config();
+
+export default {
+  solidity: "0.8.21",
+  networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545"
+    }
+  }
+};
