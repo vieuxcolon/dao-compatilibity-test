aa8a3d8af7848cc3c40249e7ab49cac3740a044e
vieuxcolon <vieuxcolon@hotmail.fr>
2026-01-19 12:59:12 +0100

    Update hardhat.config.mjs

--- hardhat.config.mjs
+++ hardhat.config.mjs
@@ -5,9 +5,21 @@ dotenv.config();
 
 export default {
   solidity: "0.8.21",
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  },
   networks: {
     localhost: {
       url: "http://127.0.0.1:8545"
+    },
+    hardhat: {
+      chainId: 1337
     }
+  },
+  mocha: {
+    timeout: 20000
   }
 };
