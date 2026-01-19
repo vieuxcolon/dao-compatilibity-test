1b18aed9283887af6a4aa93ef7ddea8355ce58f4
vieuxcolon <vieuxcolon@hotmail.fr>
2026-01-19 10:49:07 +0100

    Update hardhat.config.mjs

--- hardhat.config.mjs
+++ hardhat.config.mjs
@@ -1,10 +1,5 @@
-import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-ethers";
 
 export default {
-  solidity: "0.8.20",
-  networks: {
-    localhost: {
-      url: "http://127.0.0.1:8545"
-    }
-  }
+  solidity: "0.8.20"
 };
