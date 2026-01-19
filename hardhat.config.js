a577e6e2f870b973a691e04ec8350c31ff5fd5ac
vieuxcolon <vieuxcolon@hotmail.fr>
2026-01-19 02:56:06 +0100

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,15 +1,15 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 
-const RPC_URL = process.env.RPC_URL;
-const PRIVATE_KEY = process.env.PRIVATE_KEY;
-
 module.exports = {
-  solidity: "0.8.21",
+  solidity: "0.8.20",
   networks: {
+    hardhat: {
+      chainId: 1337
+    },
     localhost: {
-      url: RPC_URL,
-      accounts: PRIVATE_KEY ? [PRIVATE_KEY] : []
+      url: process.env.RPC_URL,
+      accounts: [process.env.PRIVATE_KEY]
     }
   }
 };
